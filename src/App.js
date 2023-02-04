ddb82aede1db5d887c77f478aada2b4127e2c4c7
Nilay Pophalkar <nilaypophalkar@gmail.com>
2023-02-04 16:32:47 +0530

    added FIR page and My FIR link in profile page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Login_Signup from "./pages/login-signup";
 import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Fir from "./pages/Fir";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route exact path="/login" element={<Login_Signup />} />
         <Route exact path="/" element={<Home />} />
+        <Route exact path='/fir' element={<Fir />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/Fir.jsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import Profile from './Profile';
+import './css/home.css';
+import './css/fir.css';
+const Fir = () => {
+    return (
+        <>
+            <div className="home-container">
+                <Profile />
+                <div className='home-stats'>
+                    <div className="fir-wrap">
+                        <p className='fir-title'>File FIR.</p>
+                        <div className="fir-body">
+                            <table>
+                                <tr>
+                                    <td><label className='fir-topic'>First Name:<span>*</span> </label></td>
+                                    <td ><input type="text" /></td>
+                                    <td><label className='fir-topic'>Last Name:<span>*</span></label></td>
+                                    <td><input type="text" /></td>
+                                </tr>
+                                <tr><td><label className='fir-topic'>Contact:<span>*</span></label></td>
+                                    <td><input type="number" /></td>
+                                    <td><label className='fir-topic'>Email ID:<span>*</span></label></td>
+                                    <td><input type="mail" /></td>
+                                </tr>
+                                <tr>
+                                    <td><label className='fir-topic'>Police Station:<span>*</span></label></td>
+                                    <td><input type='text' /></td>
+                                    <td><label className='fir-topic'>Date:<span>*</span></label></td>
+                                    <td><input type="date" /></td>
+                                </tr>
+                                <tr>
+                                    <td><label className='fir-topic'>Address:<span>*</span></label></td>
+                                    <td colSpan={3}><textarea name="" id="" rows="4" className='textarea'></textarea></td>
+                                </tr>
+                                <tr><td><label className='fir-topic'>Complaint:<span>*</span></label></td>
+                                    <td colSpan={3}><textarea name="" id="" rows="4" className='textarea'></textarea></td>
+                                </tr>
+                                <tr>
+                                    <td> <label className='fir-topic'>Upload Signature:<span>*</span></label></td>
+                                    <td>
+                                        <input type="file" />
+                                    </td>
+                                </tr>
+                            </table>
+
+                            <div className="fir-confirmation">
+                                <input type="checkbox" />
+                                <p className='fir-confirm-text'>I  certify that the information provided by me in above form is accurate and correct as per best of my knowledge. I understand that withholding of information or giving false information is a criminal offense and may result in legal action against me.</p>
+                            </div>
+                            <div className="fir-btnGroup">
+                                <button>Reset</button>
+                                <button>Submit</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Fir
