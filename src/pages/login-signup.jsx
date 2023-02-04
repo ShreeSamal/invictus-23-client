f2539c9922f2d91d750291beb02736e9a1739d90
ShreeSamal <shree.samal1502@gmail.com>
2023-02-04 09:36:33 +0530

    added session creation

--- src/pages/login-signup.jsx
+++ src/pages/login-signup.jsx
@@ -1,10 +1,24 @@
 import React, { useState } from "react";
 import './css/login_signup.css';
+import {useCookies} from 'react-cookie';
+var CryptoJS = require('crypto-js');
+
 export default function Login_Signup() {
     const [name, setName] = useState("");
     const [contact, setContact] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [cookies, setCookie,removeCookie] = useCookies('user');
+
+    const handle = () => {
+        if(cookies){
+        removeCookie('user');
+        }
+        var user = { email: email }
+        var ciphertext = CryptoJS.AES.encrypt(JSON.stringify(user), 'my-secret-key@123').toString();
+        setCookie('user', ciphertext, { path: '/' });
+        console.log('cookie created');
+     };
 
     async function signup(e) {
         e.preventDefault();
@@ -38,26 +52,29 @@ export default function Login_Signup() {
             }),
         });
         const data = await res.json();
-        console.log(data);
+        console.log(data.message);
+        if(data.message === "Login Successful"){
+            handle();
+        }
         alert(data.message);
     }
 
     return (
-        <div class="login-main">
+        <div className="login-main">
             <input type="checkbox" id="chk" aria-hidden="true"></input>
-            <div class="signup">
+            <div className="signup">
                 <form>
-                    <label for="chk" aria-hidden="true">Sign up</label>
-                    <input type="text" onChange={(e)=>{setName(e.target.value)}} name="txt" placeholder="User name" required=""></input>
-                    <input type="text" onChange={(e)=>{setContact(e.target.value)}} name="contact" placeholder="Contact" required=""></input>
+                    <label htmlFor="chk" aria-hidden="true">Sign up</label>
+                    <input type="text" onChange={(e)=>{setName(e.target.value)}} name="txt" placeholder="name" required=""></input>
+                    <input type="tel" pattern="[0-9]{10}" onChange={(e)=>{setContact(e.target.value)}} name="contact" placeholder="Contact" required=""></input>
                     <input type="email" onChange={(e)=>{setEmail(e.target.value)}} name="email" placeholder="Email" required=""></input>
                     <input type="password" onChange={(e)=>{setPassword(e.target.value)}} name="pswd" placeholder="Password" required=""></input>
                     <button onClick={signup}>Sign up</button>
                 </form>
             </div>
-            <div class="login">
+            <div className="login">
                 <form>
-                    <label for="chk" aria-hidden="true">Login</label>
+                    <label htmlFor="chk" aria-hidden="true">Login</label>
                     <input type="email" onChange={(e)=>{setEmail(e.target.value)}} name="email" placeholder="Email" required=""></input>
                     <input type="password" onChange={(e)=>{setPassword(e.target.value)}} name="pswd" placeholder="Password" required=""></input>
                     <button onClick={login}>Login</button>
